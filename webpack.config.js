3c5ab3aec66f0ca1cd622862ee7a6e1f7e83401b
Danilo Barros <danilobjr@gmail.com>
2016-08-17 20:46:05 -0300

    basic webpack config

---
+++ webpack.config.js
@@ -0,0 +1,22 @@
+const path = require('path');
+const resolvePath = value => path.resolve(__dirname, value)
+
+module.exports = {
+    entry: resolvePath('src/index.jsx'),
+    output: {
+        path: resolvePath('build'),
+        filename: 'bundle.js'
+    },
+    module: {
+        loaders: [
+            {
+                test: /\.jsx?$/,
+                include: [resolvePath('src')],
+                loader: 'babel',
+                query: {
+                    presets: ['es2015', 'react']
+                }
+            }
+        ]
+    }
+}
