a38a2ae1ba48f41c0d3aaa2deb41fd16c2e69248
Danilo Barros <danilobjr@gmail.com>
2016-08-19 13:37:29 -0300

    Test component

---
+++ src/components/Test/Test.jsx
@@ -0,0 +1,8 @@
+import React from 'react';
+import { Paper } from './../Paper';
+
+const Test = (props) => <Paper />
+
+export {
+    Test
+};
--- src/index.jsx
+++ src/index.jsx
@@ -1,6 +1,7 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import { Test } from './components/Test';
 
 const appContainer = document.getElementById('app');
 
-ReactDOM.render(<h1>Hello, UI Kit</h1>, appContainer);
+ReactDOM.render(<Test />, appContainer);
