ddd40670189da2ce01c675f8a839f15aa63e9130
Danilo Barros <danilobjr@gmail.com>
2016-08-21 18:35:36 -0300

    removing stringOrCheckboxLabel propType

--- src/utils/propTypes.js
+++ src/utils/propTypes.js
@@ -6,14 +6,7 @@ import { CheckboxLabel } from 'components'
 const propTypes = {
     fontColors: PropTypes.oneOf(objectValues(colors.font)),
     backgroundColors: PropTypes.oneOf(objectValues(colors.background)),
-    elevation: PropTypes.oneOf([0,1,2,3,4,5]),
-    stringOrCheckboxLabel: (props, propName, componentName) => {
-        const child = props[propName];
-
-        if (child.type !== CheckboxLabel && typeof child !== 'string') {
-            return new Error(`Children supplied to '${componentName}' can only be an instance of CheckboxLabel or a string.`);
-        }
-    }
+    elevation: PropTypes.oneOf([0,1,2,3,4,5])
 };
 
 export {
