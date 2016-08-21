f398255979b279dd6af034cbf6fb25c61bf6df5e
Danilo Barros <danilobjr@gmail.com>
2016-08-21 18:09:26 -0300

    new prop validation, stringOrCheckboxLabel

--- src/utils/propTypes.js
+++ src/utils/propTypes.js
@@ -1,11 +1,19 @@
-import { PropTypes } from 'react'
+import { PropTypes, Children } from 'react'
 import { objectValues } from 'utils'
 import { colors } from 'styles'
+import { CheckboxLabel } from 'components'
 
 const propTypes = {
     fontColors: PropTypes.oneOf(objectValues(colors.font)),
     backgroundColors: PropTypes.oneOf(objectValues(colors.background)),
-    elevation: PropTypes.oneOf([0,1,2,3,4,5])
+    elevation: PropTypes.oneOf([0,1,2,3,4,5]),
+    stringOrCheckboxLabel: (props, propName, componentName) => {
+        const child = props[propName];
+
+        if (child.type !== CheckboxLabel && typeof child !== 'string') {
+            return new Error(`Children supplied to '${componentName}' can only be an instance of CheckboxLabel or a string.`);
+        }
+    }
 };
 
 export {
