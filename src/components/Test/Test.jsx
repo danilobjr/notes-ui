aad927c4a1004d7b9678421c9ad071af178f90c3
Danilo Barros <danilobjr@gmail.com>
2016-08-22 00:55:08 -0300

    testing FloatingInput

--- src/components/Test/Test.jsx
+++ src/components/Test/Test.jsx
@@ -1,9 +1,18 @@
 import React from 'react'
-import { Layout, Card, Checkbox } from 'components'
+import { Layout, Card, Checkbox, FloatingInput } from 'components'
 import { colors } from 'styles'
 
 const Test = (props) => 
     <Layout>
+        <FloatingInput
+            style={{ 
+                width: '296px',
+                marginBottom: '50px' 
+            }}
+            icon="NOT-WORKING"
+            placeholder="Search Notes" 
+        />
+
         <Card
             title="UI Design Tools"
             color={colors.background.blue}
@@ -11,6 +20,7 @@ const Test = (props) =>
             elevation={2}
         >
             <Checkbox>Photoshop</Checkbox>
+            <Checkbox>Inkscape</Checkbox>
         </Card>
     </Layout>
 
