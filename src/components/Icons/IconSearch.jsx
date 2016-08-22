40e9368b0c4dbc58bd897a06f6a19cf2fef28a38
Danilo Barros <danilobjr@gmail.com>
2016-08-22 12:33:56 -0300

    default style for IconSearch

--- src/components/Icons/IconSearch.jsx
+++ src/components/Icons/IconSearch.jsx
@@ -1,11 +1,19 @@
 import React, { PropTypes } from 'react'
 import { colors } from 'styles'
 
+const componentStyle = {
+    base: {
+        width: '20px'
+    }
+};
+
+const getStyle = (style, props) => Object.assign({}, style.base, props.style)
+
 const IconSearch = (props) => 
     <svg
         xmlns="http://www.w3.org/2000/svg"
         viewBox="0 0 255.99996 255.99993"
-        style={props.style}
+        style={getStyle(componentStyle, props)}
     >
         <path
             d="m 290.50257,436.78372 a 81.473053,81.473053 0 0 0 -24.71256,58.44299 81.473053,81.473053 0 0 0 81.47452,81.47459 81.473053,81.473053 0 0 0 81.47203,-81.4721 81.473053,81.473053 0 0 0 -81.47451,-81.4745 81.473053,81.473053 0 0 0 -56.75948,23.02902 z m 120.69138,122.37489 89.25718,89.25719 -89.25718,-89.25719 z"
