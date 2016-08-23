8e8cbaa515b99a0748302e0240ae7564a386e5cb
Danilo Barros <danilobjr@gmail.com>
2016-08-23 20:46:39 -0300

    basic component drawer

---
+++ src/components/Drawer/Drawer.jsx
@@ -0,0 +1,43 @@
+import React, { Component, PropTypes } from 'react'
+import Radium from 'radium'
+import { Paper } from 'components'
+
+const componentStyle = {
+    base: {
+        position: 'absolute',
+        left: -296,
+        top: 0,
+        width: 296,
+        height: '100vh'
+    },
+    open: {
+        left: 0
+    }
+};
+
+const getStyle = (style, props) => ([
+    style.base,
+    props.open && style.open,
+    props.style
+])
+
+@Radium
+class Drawer extends Component {
+    render() {
+        return (
+            <Paper style={getStyle(componentStyle, this.props)}></Paper>
+        );
+    }
+}
+
+Drawer.propTypes = {
+    open: PropTypes.bool
+};
+
+Drawer.defaultProps = {
+    open: false
+};
+
+export {
+    Drawer
+}
