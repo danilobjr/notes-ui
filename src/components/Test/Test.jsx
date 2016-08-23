2878c11f3488c82efe2f7f40aaf8abd0e1d4046f
Danilo Barros <danilobjr@gmail.com>
2016-08-23 12:06:32 -0300

    testing

--- src/components/Test/Test.jsx
+++ src/components/Test/Test.jsx
@@ -16,94 +16,120 @@ const style = {
     },
     checklistItem: {
         marginBottom: '22px'
+    },
+    strikedItem: {
+        fontStyle: 'italic',
+        color: colors.font.lightGray,
+        textDecoration: 'line-through'
     }
 };
 
-const Test = (props) => 
-    <Layout>
-        <FloatingInput
-            style={{ 
-                width: '296px',
-                marginBottom: '50px' 
-            }}
-            leftComponent={<Icon name="search" style={{ padding: '0 14px 0 20px' }} color={colors.font.lightGray} />}
-            placeholder="Search Notes" 
-        />
+class Test extends React.Component { 
+    constructor(props) {
+        super(props);
+    
+        this.state = {
+            textStyle: {}
+        };
+    }
 
-        <div style={style.cardList}>
-            <Card
-                style={style.card}
-                title="UI Design Tools"
-                color={colors.background.blue}
-            >
-                <Checkbox style={style.checklistItem}>Photoshop</Checkbox>
-                <Checkbox style={style.checklistItem}>Inkscape</Checkbox>
-                <Checkbox
-                    onCheck={() => console.log('checked')}
-                    onUncheck={() => console.log('unchecked')}
+    render() {
+        return (
+            <Layout>
+                <FloatingInput
+                    style={{ 
+                        width: '296px',
+                        marginBottom: '50px' 
+                    }}
+                    leftComponent={<Icon name="search" style={{ padding: '0 14px 0 20px' }} color={colors.font.lightGray} />}
+                    placeholder="Search Notes" 
+                />
+        
+                <div style={style.cardList}>
+                    <Card
+                        style={style.card}
+                        title="UI Design Tools"
+                        color={colors.background.blue}
+                    >
+                        <Checkbox style={style.checklistItem}>Photoshop</Checkbox>
+                        <Checkbox style={style.checklistItem}>Inkscape</Checkbox>
+                        <Checkbox
+                            onCheck={this.setLineThroughText.bind(this)}
+                            onUncheck={this.setNoStyle.bind(this)}
+                        >
+                            <div style={this.state.textStyle}>Sketch</div>
+                        </Checkbox>
+                    </Card>
+        
+                    <Card
+                        style={style.card}
+                        title="Cream Pie Recipe"
+                        image="http://www.carlsbad-convention.cz/images/gastronomie/04.jpg"
+                    >
+                        <Checkbox style={style.checklistItem}>Honey</Checkbox>
+                        <Checkbox style={style.checklistItem}>Egg Yolk</Checkbox>
+                        <Checkbox style={style.checklistItem}>Sugar</Checkbox>
+                        <Checkbox>Olive Oil</Checkbox>
+                    </Card>
+        
+                    <Card
+                        style={style.card}
+                        title="Weekly Grocery"
+                        color={colors.background.green}
+                    >
+                        <Checkbox style={style.checklistItem}>Avocado</Checkbox>
+                        <Checkbox style={style.checklistItem}>Tomato</Checkbox>
+                        <Checkbox style={style.checklistItem}>Ceasar Salad</Checkbox>
+                        <Checkbox style={style.checklistItem}>2x Tissue</Checkbox>
+                        <Checkbox>Sunblock</Checkbox>
+                    </Card>
+        
+                    <Card
+                        style={style.card}
+                        title="Newsletter Draft"
+                    >
+                        Lorem ipsum dolor sit amet, consectetur adipiscing elit. 
+                        Phasellus nec maximus nunc. Nullam consequat rhoncus dictum. Pellentesque habitant morbi tristique.
+                    </Card>
+                </div>
+        
+                <IconButton
+                    iconName="search"
+                    flat
+                    onClick={() => console.log('IconButton clicked')} 
+                />
+        
+                <IconButton
+                    iconName="search"
+                    onClick={() => console.log('IconButton clicked')} 
+                />
+        
+                <Button
+                    style={{ marginBottom: '50px' }}
+                    onClick={() => console.log('Save clicked')}
                 >
-                    Sketch
-                </Checkbox>
-            </Card>
+                    Save
+                </Button>
+        
+                <Button
+                    style={{ marginBottom: '50px', marginLeft: '10px' }}
+                    flat
+                    onClick={() => console.log('Cancel clicked')}
+                >
+                    Cancel
+                </Button>
+            </Layout>
+        );
+    }
 
-            <Card
-                style={style.card}
-                title="Cream Pie Recipe"
-                image="http://www.carlsbad-convention.cz/images/gastronomie/04.jpg"
-            >
-                <Checkbox style={style.checklistItem}>Honey</Checkbox>
-                <Checkbox style={style.checklistItem}>Egg Yolk</Checkbox>
-                <Checkbox style={style.checklistItem}>Sugar</Checkbox>
-                <Checkbox>Olive Oil</Checkbox>
-            </Card>
+    setLineThroughText() {
+        this.setState({ textStyle: style.strikedItem });
+    }
 
-            <Card
-                style={style.card}
-                title="Weekly Grocery"
-                color={colors.background.green}
-            >
-                <Checkbox style={style.checklistItem}>Avocado</Checkbox>
-                <Checkbox style={style.checklistItem}>Tomato</Checkbox>
-                <Checkbox style={style.checklistItem}>Ceasar Salad</Checkbox>
-                <Checkbox style={style.checklistItem}>2x Tissue</Checkbox>
-                <Checkbox>Sunblock</Checkbox>
-            </Card>
-
-            <Card
-                style={style.card}
-                title="Newsletter Draft"
-            >
-                Lorem ipsum dolor sit amet, consectetur adipiscing elit. 
-                Phasellus nec maximus nunc. Nullam consequat rhoncus dictum. Pellentesque habitant morbi tristique.
-            </Card>
-        </div>
-
-        <IconButton
-            iconName="search"
-            flat
-            onClick={() => console.log('IconButton clicked')} 
-        />
-
-        <IconButton
-            iconName="search"
-            onClick={() => console.log('IconButton clicked')} 
-        />
-
-        <Button
-            style={{ marginBottom: '50px' }}
-            onClick={() => console.log('Save clicked')}
-        >
-            Save
-        </Button>
-
-        <Button
-            style={{ marginBottom: '50px', marginLeft: '10px' }}
-            flat
-            onClick={() => console.log('Cancel clicked')}
-        >
-            Cancel
-        </Button>
-    </Layout>
+    setNoStyle() {
+        this.setState({ textStyle: {} });
+    }
+}
 
 export {
     Test
