23eb9d04338e97f65408d77720673d90397be732
Danilo Barros <danilobjr@gmail.com>
2016-08-23 20:25:41 -0300

    changing elevation level 1

--- src/styles/elevation.js
+++ src/styles/elevation.js
@@ -3,9 +3,7 @@ const elevation = {
         boxShadow: 'none'
     },
     1: {
-        boxShadow: `0 2px 2px 0 rgba(0, 0, 0, 0.14),
-                    0 1px 5px 0 rgba(0, 0, 0, 0.12),
-                    0 3px 1px -2px rgba(0, 0, 0, 0.2)`
+        boxShadow: '0 6px 16px -5px rgba(0, 0, 0, 0.2)'
     },
     2: {
         boxShadow: '0 12px 32px -9px rgba(0, 0, 0, 0.2)'
