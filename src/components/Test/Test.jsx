7d1af9f1bb55dc67def66046a80f8baaa148d09d
Danilo Barros <danilobjr@gmail.com>
2016-08-24 20:20:24 -0300

    FAB with plus icon

--- src/components/Test/Test.jsx
+++ src/components/Test/Test.jsx
@@ -13,11 +13,22 @@ const style = {
         alignItems: 'center'
     },
     layoutBody: {
-        padding: 32
+        display: 'flex',
+        width: '100vw'
+    },
+    cardsView: {
+        flex: 1,
+        display: 'flex',
+        padding: 32,
+        justifyContent: 'center'
     },
     drawer: {
         height: 'calc(100vh - 64px)',
-        top: 64
+        top: 64,
+        width: 0
+    },
+    drawerOpen: {
+        width: 296
     },
     floatingSearch: {
         width: 296,
@@ -58,92 +69,98 @@ class Test extends React.Component {
                     <AppBar appTitle="JS Channel Notes" onToggleMenuClick={this.toggleMenu.bind(this)} />
                     
                     <div style={style.layoutBody}>
-                        <Drawer style={style.drawer} open={this.state.menuIsOpen}>
+                        <Drawer style={this.getDrawerStyle()}>
                             <DrawerItem iconName="search" text="Text" onClick={() => console.log('Filter by Notes of type Text')} />
                         </Drawer>
 
-                        <FloatingInput
-                            style={style.floatingSearch}
-                            leftComponent={<Icon name="search" style={{ padding: '0 14px 0 20px' }} color={colors.font.lightGray} />}
-                            placeholder="Search Notes" 
-                        />
-                
-                        <div style={style.cardList}>
-                            <Card
-                                style={style.card}
-                                title="UI Design Tools"
-                                color={colors.background.blue}
-                            >
-                                <Checkbox style={style.checklistItem}>Photoshop</Checkbox>
-                                <Checkbox style={style.checklistItem}>Inkscape</Checkbox>
-                                <Checkbox
-                                    onCheck={this.setLineThroughText.bind(this)}
-                                    onUncheck={this.setNoStyle.bind(this)}
+                        <div style={style.cardsView}>
+                            <div style={style.centeredContainer}>
+                                <FloatingInput
+                                    style={style.floatingSearch}
+                                    leftComponent={<Icon name="search" style={{ padding: '0 14px 0 20px' }} color={colors.font.lightGray} />}
+                                    placeholder="Search Notes" 
+                                />
+                        
+                                <div style={style.cardList}>
+                                    <Card
+                                        style={style.card}
+                                        title="UI Design Tools"
+                                        color={colors.background.blue}
+                                    >
+                                        <Checkbox style={style.checklistItem}>Photoshop</Checkbox>
+                                        <Checkbox style={style.checklistItem}>Inkscape</Checkbox>
+                                        <Checkbox
+                                            onCheck={this.setLineThroughText.bind(this)}
+                                            onUncheck={this.setNoStyle.bind(this)}
+                                        >
+                                            <div style={this.state.textStyle}>Sketch</div>
+                                        </Checkbox>
+                                    </Card>
+                        
+                                    <Card
+                                        style={style.card}
+                                        title="Cream Pie Recipe"
+                                        image="http://www.carlsbad-convention.cz/images/gastronomie/04.jpg"
+                                    >
+                                        <Checkbox style={style.checklistItem}>Honey</Checkbox>
+                                        <Checkbox style={style.checklistItem}>Egg Yolk</Checkbox>
+                                        <Checkbox style={style.checklistItem}>Sugar</Checkbox>
+                                        <Checkbox>Olive Oil</Checkbox>
+                                    </Card>
+                        
+                                    <Card
+                                        style={style.card}
+                                        title="Weekly Grocery"
+                                        color={colors.background.green}
+                                    >
+                                        <Checkbox style={style.checklistItem}>Avocado</Checkbox>
+                                        <Checkbox style={style.checklistItem}>Tomato</Checkbox>
+                                        <Checkbox style={style.checklistItem}>Ceasar Salad</Checkbox>
+                                        <Checkbox style={style.checklistItem}>2x Tissue</Checkbox>
+                                        <Checkbox>Sunblock</Checkbox>
+                                    </Card>
+                        
+                                    <Card
+                                        style={style.card}
+                                        title="Newsletter Draft"
+                                    >
+                                        Lorem ipsum dolor sit amet, consectetur adipiscing elit. 
+                                        Phasellus nec maximus nunc. Nullam consequat rhoncus dictum. Pellentesque habitant morbi tristique.
+                                    </Card>
+                                </div>
+
+                                {/*
+                                <IconButton
+                                    iconName="search"
+                                    flat
+                                    onClick={() => console.log('IconButton clicked')} 
+                                />
+                        
+                                <IconButton
+                                    iconName="search"
+                                    onClick={() => console.log('IconButton clicked')} 
+                                />
+                        
+                                <Button
+                                    style={{ marginBottom: '50px' }}
+                                    onClick={() => console.log('Save clicked')}
                                 >
-                                    <div style={this.state.textStyle}>Sketch</div>
-                                </Checkbox>
-                            </Card>
-                
-                            <Card
-                                style={style.card}
-                                title="Cream Pie Recipe"
-                                image="http://www.carlsbad-convention.cz/images/gastronomie/04.jpg"
-                            >
-                                <Checkbox style={style.checklistItem}>Honey</Checkbox>
-                                <Checkbox style={style.checklistItem}>Egg Yolk</Checkbox>
-                                <Checkbox style={style.checklistItem}>Sugar</Checkbox>
-                                <Checkbox>Olive Oil</Checkbox>
-                            </Card>
-                
-                            <Card
-                                style={style.card}
-                                title="Weekly Grocery"
-                                color={colors.background.green}
-                            >
-                                <Checkbox style={style.checklistItem}>Avocado</Checkbox>
-                                <Checkbox style={style.checklistItem}>Tomato</Checkbox>
-                                <Checkbox style={style.checklistItem}>Ceasar Salad</Checkbox>
-                                <Checkbox style={style.checklistItem}>2x Tissue</Checkbox>
-                                <Checkbox>Sunblock</Checkbox>
-                            </Card>
-                
-                            <Card
-                                style={style.card}
-                                title="Newsletter Draft"
-                            >
-                                Lorem ipsum dolor sit amet, consectetur adipiscing elit. 
-                                Phasellus nec maximus nunc. Nullam consequat rhoncus dictum. Pellentesque habitant morbi tristique.
-                            </Card>
+                                    Save
+                                </Button>
+                        
+                                <Button
+                                    style={{ marginBottom: '50px', marginLeft: '10px' }}
+                                    flat
+                                    onClick={() => console.log('Cancel clicked')}
+                                >
+                                    Cancel
+                                </Button>
+                                */}
+                            </div>
                         </div>
-                
-                        <IconButton
-                            iconName="search"
-                            flat
-                            onClick={() => console.log('IconButton clicked')} 
-                        />
-                
-                        <IconButton
-                            iconName="search"
-                            onClick={() => console.log('IconButton clicked')} 
-                        />
-                
-                        <Button
-                            style={{ marginBottom: '50px' }}
-                            onClick={() => console.log('Save clicked')}
-                        >
-                            Save
-                        </Button>
-                
-                        <Button
-                            style={{ marginBottom: '50px', marginLeft: '10px' }}
-                            flat
-                            onClick={() => console.log('Cancel clicked')}
-                        >
-                            Cancel
-                        </Button>
                     </div>
 
-                    <FAB iconName="search" onClick={() => console.log('FAB clicked')} />
+                    <FAB iconName="plus" onClick={() => console.log('FAB clicked')} />
                 </Layout>
             </div>
         );
@@ -159,6 +176,16 @@ class Test extends React.Component {
 
     toggleMenu() {
         this.setState({ menuIsOpen: !this.state.menuIsOpen });
+    }
+
+    getDrawerStyle() {
+        const { menuIsOpen } = this.state;
+
+        if (menuIsOpen) {
+            return Object.assign({}, style.drawer, style.drawerOpen);
+        }
+
+        return style.drawer;
     }
 }
 
