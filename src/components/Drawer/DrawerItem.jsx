8479445f63f653bd3f12822870e71cae59116dc6
Danilo Barros <danilobjr@gmail.com>
2016-08-23 21:52:56 -0300

    new DrawerItem component

---
+++ src/components/Drawer/DrawerItem.jsx
@@ -0,0 +1,64 @@
+import React, { Component, PropTypes } from 'react'
+import Radium from 'radium'
+import { Icon } from 'components'
+import { colors } from 'styles'
+
+const componentStyle = {
+    base: {
+        display: 'flex',
+        height: 54,
+        alignItems: 'center',
+        cursor: 'pointer',
+        userSelect: 'none',
+
+        ':hover': {
+            backgroundColor: colors.grayHover
+        },
+
+        ':active': {
+            backgroundColor: colors.grayActive
+        }
+    },
+    icon: {
+        marginLeft: 25
+    },
+    text: {
+        marginLeft: 25
+    }
+};
+
+const getStyle = (style, props) => ([
+    style.base,
+    props.style
+])
+
+@Radium
+class DrawerItem extends Component {
+    render() {
+        return (
+            <div style={getStyle(componentStyle, this.props)}>
+                {this.renderIcon()}
+                <span style={componentStyle.text}>{this.props.text}</span>
+            </div>
+        );
+    }
+
+    renderIcon() {
+        const { iconName } = this.props;
+
+        if (!iconName) {
+            return null;
+        }
+
+        return <Icon style={componentStyle.icon} name={iconName} />;
+    }
+}
+
+DrawerItem.propTypes = {
+    iconName: PropTypes.string,
+    text: PropTypes.string
+};
+
+export {
+    DrawerItem
+}
