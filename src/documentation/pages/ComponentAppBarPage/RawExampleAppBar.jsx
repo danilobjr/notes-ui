f33782daabb26884c6315c5100fa64c0a818abf5
Danilo Barros <danilobjr@gmail.com>
2016-08-27 16:49:07 -0300

    RawExampleAppBar is complete

--- src/documentation/pages/ComponentAppBarPage/RawExampleAppBar.jsx
+++ src/documentation/pages/ComponentAppBarPage/RawExampleAppBar.jsx
@@ -5,26 +5,38 @@ import { colors } from 'styles'
 
 const componentStyle = {
     base: {
-        width: '100%',
-        backgroundColor: colors.background.blue
+        backgroundColor: '#3498db'
     }
 };
+
+const description = "This is how you get AppBar if you don't pass any props. " +
+    "I just change the colors to you see better. " +
+    "It have white as its default background color and the toggle drawer icon is gray.";
 
 const RawExampleAppBar = (props) => 
     <Example
         title="Raw example"
-        description="This is how you get AppBar if you don't pass any props."
+        description={description}
         code={`import React from 'react';
 import { AppBar } from 'AppBar';
 
+const style = {
+    base: {
+        backgrondColor: '#3498db'
+    },
+    icon: {
+        color: '#f8f8f8'
+    }
+};
+
 const AppBarExample = () => 
-    <AppBar />
+    <AppBar style={style.base} toggleDrawerIconColor={style.icon.color} />
 
 export {
     AppBarExample
 }`}
     >
-        <AppBar style={componentStyle.base} />
+        <AppBar style={componentStyle.base} toggleDrawerIconColor={colors.font.white} />
     </Example>
 
 export {
