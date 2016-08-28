a18e7fc1b356b0cb08821b30a15aa45bbdb28943
Danilo Barros <danilobjr@gmail.com>
2016-08-28 00:37:02 -0300

    new PropertiesAppBar component

---
+++ src/documentation/pages/ComponentAppBarPage/PropertiesAppBar.jsx
@@ -0,0 +1,39 @@
+import React from 'react'
+
+const componentStyle = {
+    base: {
+        
+    }
+};
+
+const PropertiesAppBar = (props) => 
+    <div>
+        <table>
+            <thead>
+                <tr>
+                    <th>Head</th>
+                    <th>Head</th>
+                    <th>Head</th>
+                    <th>Head</th>
+                </tr>
+            </thead>
+            <tbody>
+                <tr>
+                    <td>Data</td>
+                    <td>Data</td>
+                    <td>Data</td>
+                    <td>Data</td>
+                </tr>
+                <tr>
+                    <td>Data</td>
+                    <td>Data</td>
+                    <td>Data</td>
+                    <td>Data</td>
+                </tr>
+            </tbody>
+        </table>
+    </div>
+
+export {
+    PropertiesAppBar
+}
