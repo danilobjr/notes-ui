25c6bc0390d1dd49e30972728174542bea6b0258
Danilo Barros <danilobjr@gmail.com>
2016-08-28 00:39:47 -0300

    primitive properties markup

--- src/documentation/pages/ComponentAppBarPage/ComponentAppBarPage.jsx
+++ src/documentation/pages/ComponentAppBarPage/ComponentAppBarPage.jsx
@@ -1,16 +1,21 @@
 import React, { Component } from 'react'
-import { Page, P } from 'documentation/common'
+import { Page, Paragraph } from 'documentation/common'
+import { PropertiesAppBar } from './PropertiesAppBar'
 import { RawExampleAppBar } from './RawExampleAppBar'
 
 class ComponentAppBarPage extends Component {
     render() {
         return (
             <Page headerText="App Bar">
-                <P>App bar is a special bar on top of an app (justifying its name) containing a title and actions.</P>
+                <Paragraph>App bar is a special bar on top of an app (justifying its name) containing a title and actions.</Paragraph>
 
                 <h3>Examples</h3>
 
                 <RawExampleAppBar />
+
+                <h3>Properties</h3>
+
+                <PropertiesAppBar />
             </Page>
         );
     }
