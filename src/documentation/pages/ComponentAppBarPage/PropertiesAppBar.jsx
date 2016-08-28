3f5f5c0a6aa26ebd2f40b9835b0c836ef4c869a8
Danilo Barros <danilobjr@gmail.com>
2016-08-28 17:16:56 -0300

    documenting AppBar properties

--- src/documentation/pages/ComponentAppBarPage/PropertiesAppBar.jsx
+++ src/documentation/pages/ComponentAppBarPage/PropertiesAppBar.jsx
@@ -3,7 +3,30 @@ import { PropertyTable, Property } from 'documentation/common'
 
 const PropertiesAppBar = (props) => 
     <PropertyTable>
-        <Property name="appTitle" type="string" description="Title" />
+        <Property
+            name="title"
+            type="string"
+            description="Title displayed in app bar." 
+        />
+
+        <Property
+            name="hideToggleDrawerIconButton"
+            type="bool"
+            defaultValue="false"
+            description="Hide default button." 
+        />
+
+        <Property
+            name="toggleDrawerIconColor"
+            type="string"
+            description="Color of default button. Can be a string, rgb, hex or hsl value." 
+        />
+
+        <Property
+            name="onToggleDrawerClick"
+            type="function"
+            description="A function that will be triggered when the default button is clicked." 
+        />
     </PropertyTable>
 
 export {
