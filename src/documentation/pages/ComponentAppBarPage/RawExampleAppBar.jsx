b71ae6a220e8a5c0fe64893315055e5c405b545c
Danilo Barros <danilobjr@gmail.com>
2016-08-28 17:00:30 -0300

    refactoring

--- src/documentation/pages/ComponentAppBarPage/RawExampleAppBar.jsx
+++ src/documentation/pages/ComponentAppBarPage/RawExampleAppBar.jsx
@@ -6,19 +6,17 @@ import { colors } from 'styles'
 const description = "This is how you get AppBar if you don't pass any props. " +
     "It have white as its default background color and the toggle drawer icon is gray.";
 
+const code = `import React from 'react'
+import { AppBar } from 'js-channel-ui-kit'
+
+const AppBarExample = () => 
+    <AppBar />`;
+
 const RawExampleAppBar = (props) => 
     <Example
         title="Raw example"
         description={description}
-        code={`import React from 'react';
-import { AppBar } from 'AppBar';
-
-const AppBarExample = () => 
-    <AppBar />
-
-export {
-    AppBarExample
-}`}
+        code={code}
     >
         <AppBar />
     </Example>
--- src/documentation/pages/ComponentAppBarPage/TitleAndClickExampleAppBar.jsx
+++ src/documentation/pages/ComponentAppBarPage/TitleAndClickExampleAppBar.jsx
@@ -2,20 +2,18 @@ import React from 'react'
 import { Example } from 'documentation/common'
 import { AppBar } from 'components'
 
+const description = "You can have a Title and a custom behavior in toggle drawer icon button.";
 
-const code = `import { AppBar } from 'js-channel-ui-kit'
+const code = `import React from 'react'
+import { AppBar } from 'js-channel-ui-kit'
 
 const AppBarExample = props =>
-    <AppBar title="Page" onToggleDrawerClick={() => alert('Clicked')} />
-
-export {
-    AppBarExample
-}`
+    <AppBar title="Page" onToggleDrawerClick={() => alert('Clicked')} />`;
 
 const TitleAndClickExampleAppBar = (props) => 
     <Example
         title="Title and Click"
-        description="You can have a Title and a custom behavior in toggle drawer icon button."
+        description={description}
         code={code}
     >
         <AppBar title="Page" onToggleDrawerClick={() => alert('Clicked')} />
