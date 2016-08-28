e0d29584b3e3bfb8840694132aa7ee2b579a0122
Danilo Barros <danilobjr@gmail.com>
2016-08-28 16:09:44 -0300

    removing colors

--- src/documentation/pages/ComponentAppBarPage/RawExampleAppBar.jsx
+++ src/documentation/pages/ComponentAppBarPage/RawExampleAppBar.jsx
@@ -3,14 +3,7 @@ import { Example } from 'documentation/common'
 import { AppBar } from 'components'
 import { colors } from 'styles'
 
-const componentStyle = {
-    base: {
-        backgroundColor: '#3498db'
-    }
-};
-
 const description = "This is how you get AppBar if you don't pass any props. " +
-    "I just change the colors to you see better. " +
     "It have white as its default background color and the toggle drawer icon is gray.";
 
 const RawExampleAppBar = (props) => 
@@ -20,23 +13,14 @@ const RawExampleAppBar = (props) =>
         code={`import React from 'react';
 import { AppBar } from 'AppBar';
 
-const style = {
-    base: {
-        backgrondColor: '#3498db'
-    },
-    icon: {
-        color: '#f8f8f8'
-    }
-};
-
 const AppBarExample = () => 
-    <AppBar style={style.base} toggleDrawerIconColor={style.icon.color} />
+    <AppBar />
 
 export {
     AppBarExample
 }`}
     >
-        <AppBar style={componentStyle.base} toggleDrawerIconColor={colors.font.white} />
+        <AppBar />
     </Example>
 
 export {
