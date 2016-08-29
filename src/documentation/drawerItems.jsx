80054ca7a785f6ab466f1887db98829aa4155531
Danilo Barros <danilobjr@gmail.com>
2016-08-29 15:17:14 -0300

    fixing components

--- src/documentation/drawerItems.jsx
+++ src/documentation/drawerItems.jsx
@@ -1,10 +1,10 @@
 import React from 'react'
-import { DrawerItem } from 'components'
+import { DrawerSubitemsContainer, DrawerItem } from 'components'
 
 const drawerItems = [
-    <DrawerItem key="1" text="Components">
+    <DrawerSubitemsContainer key="1" text="Components">
         <DrawerItem subitem text="App Bar" />
-    </DrawerItem>
+    </DrawerSubitemsContainer>
 ];
 
 export {
