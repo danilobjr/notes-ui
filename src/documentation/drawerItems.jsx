73077d3d004d807b308355a2f035cd328e348153
Danilo Barros <danilobjr@gmail.com>
2016-08-29 11:23:10 -0300

    drawer items in a separate file

---
+++ src/documentation/drawerItems.jsx
@@ -0,0 +1,12 @@
+import React from 'react'
+import { DrawerItem } from 'components'
+
+const drawerItems = [
+    <DrawerItem key="1" text="Components">
+        <DrawerItem subitem text="App Bar" />
+    </DrawerItem>
+];
+
+export {
+    drawerItems
+}
