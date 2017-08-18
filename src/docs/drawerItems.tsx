13dd28ee2dcaedceca41641a1bf343404e1decde
Danilo Barros <danilobjr@gmail.com>
2017-08-18 00:15:05 -0300

    fix(drawerItems): expanded as initial state

--- src/docs/drawerItems.tsx
+++ src/docs/drawerItems.tsx
@@ -17,7 +17,11 @@ import {
 } from 'docs/pages';
 
 export const getDrawerItems = setCurrentPage => (
-  <DrawerSubitemsContainer key="1" text="Components">
+  <DrawerSubitemsContainer
+    key="1"
+    expanded
+    text="Components"
+  >
     <DrawerItem subitem text="App Bar" onClick={() => setCurrentPage(ComponentAppBarPage)} />
     <DrawerItem subitem text="Button" onClick={() => setCurrentPage(ComponentButtonPage)} />
     <DrawerItem subitem text="Card" onClick={() => setCurrentPage(ComponentCardPage)} />
