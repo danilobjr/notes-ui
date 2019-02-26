e308d95e394924c0e11cb7a02490e795e6ec2638
Danilo Barros <danilobjr@gmail.com>
2019-02-26 19:56:05 -0300

    refactor(AppBar): component

--- src/components/AppBar/AppBar.tsx
+++ src/components/AppBar/AppBar.tsx
@@ -1,47 +1,42 @@
 import * as React from 'react';
-import { omit } from 'lodash';
-import { PureComponent } from 'react';
+import { SFC } from 'react';
 import { Paper, PaperProps, IconButton } from './../';
 import { FontColor } from 'components/styles';
 
-export interface AppBarProps extends PaperProps {
+export type AppBarProps = {
+  // TODO: rename this to hideToggleDrawerButton
   hideToggleDrawerIconButton?: boolean;
   toggleDrawerIconColor?: string;
+  // TODO: rename: onClickToggleDrawerButton
   onToggleDrawerClick?: () => void;
-}
+} & PaperProps;
 
-export class AppBar extends PureComponent<AppBarProps> {
-  static defaultProps: AppBarProps = {
-    hideToggleDrawerIconButton: false,
-    toggleDrawerIconColor: FontColor.Gray,
-    onToggleDrawerClick: () => { return; },
-  };
+export const AppBar: SFC<AppBarProps> = ({
+    hideToggleDrawerIconButton,
+    toggleDrawerIconColor,
+    onToggleDrawerClick,
+    title,
+    ...otherProps
+  }) => (
+  <Paper className="nui-app-bar" {...otherProps}>
+    {!hideToggleDrawerIconButton && (
+      <IconButton
+        className="icon"
+        iconColor={toggleDrawerIconColor}
+        iconName="menu"
+        flat
+        onClick={onToggleDrawerClick}
+      />
+    )}
 
-  render() {
-    const {
-      hideToggleDrawerIconButton,
-      toggleDrawerIconColor,
-      onToggleDrawerClick,
-      title,
-      ...otherProps
-    } = omit(this.props, ['style']);
+    {!!title && (
+      <h1 className="title">{title}</h1>
+    )}
+  </Paper>
+);
 
-    return (
-      <Paper className="nui-app-bar" {...otherProps}>
-        {!hideToggleDrawerIconButton && (
-          <IconButton
-            className="icon"
-            iconColor={toggleDrawerIconColor}
-            iconName="menu"
-            flat
-            onClick={onToggleDrawerClick}
-          />
-        )}
-
-        {!!title && (
-          <h1 className="title">{title}</h1>
-        )}
-      </Paper>
-    );
-  }
-}
+AppBar.defaultProps = {
+  hideToggleDrawerIconButton: false,
+  toggleDrawerIconColor: FontColor.Gray,
+  onToggleDrawerClick: () => { return; },
+};
