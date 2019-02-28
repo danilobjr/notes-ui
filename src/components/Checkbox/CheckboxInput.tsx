accdea65bc32c9b13b8a71d85d407db3d893751f
Danilo Barros <danilobjr@gmail.com>
2019-02-28 16:33:36 -0300

    refactor(CheckboxInput): use sass and SFC

--- src/components/Checkbox/CheckboxInput.tsx
+++ src/components/Checkbox/CheckboxInput.tsx
@@ -1,66 +1,23 @@
 import * as React from 'react';
-import { omit } from 'lodash';
-import { HTMLProps, PureComponent } from 'react';
-import { FontColor } from './../styles';
+import * as classNames from 'classnames';
+import { HTMLProps, SFC } from 'react';
 
-const componentStyle = {
-  input: {
-    base: {
-      width: '24px',
-      height: '24px',
-      marginRight: '16px',
-      border: '1px solid #dfdfdf',
-      borderRadius: '3px',
-    },
-    checked: {
-      backgroundColor: '#f0f0f0',
-    },
-  },
-  checkMark: {
-    base: {
-      display: 'none',
-    },
-    inputChecked: {
-      display: 'block',
-      borderBottom: `2px solid ${FontColor.LightGray}`,
-      borderLeft: `2px solid ${FontColor.LightGray}`,
-      borderTop: 'none',
-      borderRight: 'none',
-      width: '13px',
-      height: '5px',
-      transformOrigin: '0px 100% 0px',
-      transform: 'rotateZ(-45deg) translate(-1px, 14px)',
-    },
-  },
+export type CheckboxInputProps = HTMLProps<HTMLDivElement>;
+
+export const CheckboxInput: SFC<CheckboxInputProps> = ({ checked, ...otherProps }) => (
+  <div
+    className={classNames(
+      'nui-checkbox-input',
+      { '-checked': checked },
+    )}
+    {...otherProps}
+  >
+    <div className="check-mark" />
+  </div>
+);
+
+CheckboxInput.displayName = 'CheckboxInput';
+
+CheckboxInput.defaultProps = {
+  checked: false,
 };
-
-const getInputStyle = (style, props) => ([
-  style.input.base,
-  props.checked && style.input.checked,
-]);
-
-const getCheckMarkStyle = (style, props) => ([
-  style.checkMark.base,
-  props.checked && style.checkMark.inputChecked,
-]);
-
-export interface CheckboxInputProps extends HTMLProps<HTMLDivElement> { }
-
-export class CheckboxInput extends PureComponent<CheckboxInputProps, {}> {
-  static defaultProps: CheckboxInputProps = {
-    checked: false,
-  };
-
-  render() {
-    const { ...otherProps } = omit(this.props, ['checked', 'style']);
-
-    return (
-      <div
-        className="nui-checkbox-input"
-        {...otherProps}
-      >
-        <div className="nui-check-mark" />
-      </div>
-    );
-  }
-}
